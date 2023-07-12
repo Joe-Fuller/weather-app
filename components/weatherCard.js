f28d3ffcb80bc326172c82bb99f0e103d35cf255
Joe <joefuller042@gmail.com>
2023-07-12 13:13:37 +0100

    automatically fetch weather icon from saved images

--- components/weatherCard.js
+++ components/weatherCard.js
@@ -4,13 +4,16 @@ export default function WeatherCard({ weatherData }) {
   const location = weatherData.timezone;
   const temp = weatherData.current.temp;
   const type = weatherData.current.weather[0].main;
+
+  const weatherIconPath = `/${weatherData.current.weather[0].icon}.png`;
+
   return (
     <div className="bg-gray-600 w-64 h-80 p-4">
       <h2 className="text-xl font-bold mb-2">{location}</h2>
       <h2 className="text-xl font-bold mb-2">{type}</h2>
       <p className="text-xl">{temp}°C</p>
       <Image
-        src="/01d.png"
+        src={weatherIconPath}
         width={100}
         height={100}
         alt="Icon of the weather"
