5462d385bcc5bc1df3d05ec9a42c86e08a5fa16c
Joe <joefuller042@gmail.com>
2023-07-12 13:09:21 +0100

    fix weather type fetching

--- components/weatherCard.js
+++ components/weatherCard.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function WeatherCard({ weatherData }) {
   const location = weatherData.timezone;
   const temp = weatherData.current.temp;
-  const type = weatherData.current.weather.main;
+  const type = weatherData.current.weather[0].main;
   return (
     <div className="bg-gray-600 w-64 h-80 p-4">
-      <h2>{location}</h2>
-      <h2 className="text-2xl font-bold mb-2">{type}</h2>
+      <h2 className="text-xl font-bold mb-2">{location}</h2>
+      <h2 className="text-xl font-bold mb-2">{type}</h2>
       <p className="text-xl">{temp}°C</p>
       <Image
         src="/01d.png"
