eec98a7d48258eb26170d3ec8b1acc34b66c301c
Joe <joefuller042@gmail.com>
2023-07-12 12:43:36 +0100

    add sample data

--- components/weatherCard.js
+++ components/weatherCard.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 
-export default function WeatherCard({ temp, type }) {
+export default function WeatherCard({ weatherData }) {
+  const location = weatherData.timezone;
+  const temp = weatherData.current.temp;
+  const type = weatherData.current.weather.main;
   return (
     <div className="bg-gray-600 w-72 h-80 p-4">
+      <h2>{location}</h2>
       <h2 className="text-2xl font-bold mb-2">{type}</h2>
       <p className="text-xl">{temp}°C</p>
       <Image
