51fca0514d4ce1cfb5c1edc675dae1982e3bb285
Joe <joefuller042@gmail.com>
2023-07-12 11:19:02 +0100

    add fetching (cant test yet as my api key isnt working

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
 import WeatherCard from "@/components/weatherCard";
 
+async function getWeather() {
+  const res = await fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=Liverpool,uk&APPID=773c1c3ec776d5f2490ffcf71260d854"
+  );
+
+  console.log(res.json());
+  return res;
+}
+
 export default function Home() {
+  const weatherData = getWeather();
+
   return (
     <main>
       <h1>Welcome to Joe's weather app</h1>
