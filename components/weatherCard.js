a9aa1c79a73834d0efa9062b00821746e6e08738
Joe <joefuller042@gmail.com>
2023-07-12 10:49:18 +0100

    Update weather card styling

--- components/weatherCard.js
+++ components/weatherCard.js
@@ -1,8 +1,8 @@
 export default function WeatherCard({ temp, type }) {
   return (
-    <div>
-      <h2>{type}</h2>
-      <p>{temp}</p>
+    <div className="bg-gray-600 w-72 h-80 p-4">
+      <h2 className="text-2xl font-bold mb-2">{type}</h2>
+      <p className="text-xl">{temp}°C</p>
     </div>
   );
 }
