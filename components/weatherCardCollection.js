296002a6542f31ca5a2e0fb40f20d08723f295c5
Joe <joefuller042@gmail.com>
2023-07-12 14:54:24 +0100

    use index for id

--- components/weatherCardCollection.js
+++ components/weatherCardCollection.js
@@ -3,8 +3,8 @@ import WeatherCard from "./weatherCard";
 export default function WeatherCardCollection({ weatherDataCollection }) {
   return (
     <div className="flex flex-row">
-      {weatherDataCollection.map((weatherData) => (
-        <WeatherCard key={weatherData.id} weatherData={weatherData} />
+      {weatherDataCollection.map((weatherData, index) => (
+        <WeatherCard key={index} weatherData={weatherData} />
       ))}
     </div>
   );
