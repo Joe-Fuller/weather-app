8ff3bb2eadf997c60c7987512278006a5c5188e3
Joe <joefuller042@gmail.com>
2023-07-13 12:05:26 +0100

    add scrolling to forecast card collection

--- components/weatherCardCollection.js
+++ components/weatherCardCollection.js
@@ -3,9 +3,11 @@ import WeatherCard from "./weatherCard";
 export default function WeatherCardCollection({ weatherDataCollection }) {
   return (
     <div className="flex flex-row">
-      {weatherDataCollection.map((weatherData, index) => (
-        <WeatherCard key={index} weatherData={weatherData} />
-      ))}
+      <div className="overflow-x-auto flex">
+        {weatherDataCollection.map((weatherData, index) => (
+          <WeatherCard key={index} weatherData={weatherData} />
+        ))}
+      </div>
     </div>
   );
 }
