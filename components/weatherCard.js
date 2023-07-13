1f9ee2156782758baee3c44c8aa4ce1cf5152d7a
Joe <joefuller042@gmail.com>
2023-07-13 12:41:44 +0100

    add custom colours for each weather cards background

--- components/weatherCard.js
+++ components/weatherCard.js
@@ -5,7 +5,9 @@ export default function WeatherCard({ weatherData }) {
   const temp = (weatherData.main.temp - 273.15).toFixed(1);
   const type = weatherData.weather[0].main;
 
-  const weatherIconPath = `/${weatherData.weather[0].icon}.png`;
+  const weatherIcon = weatherData.weather[0].icon;
+
+  const weatherIconPath = `/${weatherIcon}.png`;
 
   const time = weatherData.dt_txt;
   let formattedTime = "";
@@ -17,8 +19,31 @@ export default function WeatherCard({ weatherData }) {
     });
   }
 
+  const backgroundColours = {
+    "01d": "bg-clearSky-day",
+    "01n": "bg-clearSky-night",
+    "02d": "bg-fewCloud-day",
+    "02n": "bg-fewClouds-night",
+    "03d": "bg-scatteredClouds-day",
+    "03n": "bg-scatteredClouds-night",
+    "04d": "bg-brokenClouds-day",
+    "04n": "bg-brokenClouds-night",
+    "09d": "bg-showerRain-day",
+    "09n": "bg-showerRain-night",
+    "10d": "bg-rain-day",
+    "10n": "bg-rain-night",
+    "11d": "bg-thunderstorm-day",
+    "11n": "bg-thunderstorm-night",
+    "13d": "bg-snow-day",
+    "13n": "bg-snow-night",
+    "50d": "bg-mist-day",
+    "50n": "bg-mist-night",
+  };
+
   return (
-    <div className="bg-gray-600 w-64 h-72 p-4 flex flex-col items-center justify-center">
+    <div
+      className={`${backgroundColours[weatherIcon]} w-64 h-72 p-4 flex flex-col items-center justify-center`}
+    >
       <h2 className="text-xl font-bold mb-2">{location}</h2>
       <h2 className="text-xl font-bold mb-2">{type}</h2>
       <p className="text-xl">{temp}°C</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,60 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: {
+          day: "#D8EAF5",
+          night: "#283149",
+        },
+        clearSky: {
+          day: "#FBD638",
+          night: "#DBA901",
+        },
+        fewClouds: {
+          day: "#F5F3B9",
+          night: "#CFC958",
+        },
+        scatteredClouds: {
+          day: "#E5E9ED",
+          night: "#8B9095",
+        },
+        brokenClouds: {
+          day: "#D1D9E6",
+          night: "#657182",
+        },
+        showerRain: {
+          day: "#91A8BA",
+          night: "#4E6A81",
+        },
+        rain: {
+          day: "#A3B8CC",
+          night: "#5B7489",
+        },
+        thunderstorm: {
+          day: "#727C8E",
+          night: "#3B4451",
+        },
+        snow: {
+          day: "#E8F0F8",
+          night: "#93A1B1",
+        },
+        mist: {
+          day: "#ECEFF4",
+          night: "#8D98A4",
+        },
       },
     },
   },
   plugins: [],
-}
+};
