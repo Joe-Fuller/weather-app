5a0a777daa848d685e819fc6367872ed8cec2b1e
Joe <joefuller042@gmail.com>
2023-07-13 17:22:22 +0100

    add box shadow to forecast cards

--- components/forecastCardCollection.js
+++ components/forecastCardCollection.js
@@ -7,7 +7,13 @@ export default function ForecastCardCollection({
 }) {
   return (
     <div className="flex flex-row">
-      <div className="overflow-x-auto flex">
+      <div
+        className="overflow-x-auto flex"
+        style={{
+          boxShadow:
+            "0px -25px 20px -20px rgba(0,0,0,0.45),0px 25px 20px -20px rgba(0,0,0,0.45)",
+        }}
+      >
         {weatherDataCollection.map((weatherData, index) => (
           <div className="w-64" key={index}>
             <ForecastCard
